679223ba9f41f4bd9bf19e3ccd4d22a293e78d58
Elisabeth Garcia <lisgarciae@gmail.com>
2023-04-12 21:23:07 -0400

    complete

--- index.js
+++ index.js
@@ -12,5 +12,17 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+    return tutorials.map((titleCasing));
 }
+
+function titleCasing(string) {
+  let words =string.split(" ")
+  for (let i=0; i<words.length;i++) {
+    words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+  }
+  return words.join(" ")
+}
+
+
+console.log(titleCased)
+
